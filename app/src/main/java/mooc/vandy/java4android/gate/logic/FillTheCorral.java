85781f81ac2c1fcc05241bac088fd4047e8ccc84
Odeniran Qozeem Banji <muhdqozeem@gmail.com>
2020-09-22 20:46:46 +0100

    finish challenge b

--- app/src/main/java/mooc/vandy/java4android/gate/logic/FillTheCorral.java
+++ app/src/main/java/mooc/vandy/java4android/gate/logic/FillTheCorral.java
@@ -9,7 +9,8 @@ import mooc.vandy.java4android.gate.ui.OutputInterface;
  * have supplied you will the code necessary to execute as an app.
  * You must fill in the missing logic below.
  */
-public class FillTheCorral {
+public class FillTheCorral
+{
     /**
      * Reference to the OutputInterface.
      */
@@ -18,10 +19,55 @@ public class FillTheCorral {
     /**
      * Constructor initializes the field.
      */
-    FillTheCorral(OutputInterface out) {
+    FillTheCorral(OutputInterface out)
+    {
         mOut = out;
     }
 
-    // TODO -- Fill your code in here
+    public void setCorralGates(Gate[] gate, Random selectDirection)
+    {
+        mOut.println("Initial gate setup");
+        for (int i = 0; i < gate.length; i++)
+        {
+            gate[i].setSwing(selectDirection.nextInt(3)-1);
+            mOut.println("Gate " + i + ": " + gate[i].toString() );
+        }
+    }
+
+    public  boolean anyCorralAvailable(Gate[] corral)
+    {
+        for (int i = 0; i < corral.length; i++)
+        {
+            if (corral[i].getSwingDirection() == Gate.IN)
+            {
+                return true;
+            }
+            else
+            {
+
+            }
+
+        }
+        return false;
+    }
+
+    public int corralSnails(Gate[] corral, Random rand)
+    {
+        int pasture = 5;
+        int attemptCount = 0;
+        int randomNumber = 0;
+        int randomGate = 0;
+        do
+        {
+            randomGate = rand.nextInt(corral.length);
+            randomNumber = rand.nextInt(pasture) + 1;
+            mOut.println(randomNumber + " are trying to move through corral " + randomGate);
+            pasture -= corral[randomGate].thru(randomNumber);
+            attemptCount++;
+        }
+        while (pasture > 0);
+        mOut.println("It took " + attemptCount + " attempts to corral all the snails");
+        return attemptCount;
+    }
 
 }
