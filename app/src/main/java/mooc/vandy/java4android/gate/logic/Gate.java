99fb9371cb0193eb1270716bd3c81baf3f2d094a
Odeniran Qozeem Banji <muhdqozeem@gmail.com>
2020-09-22 15:07:30 +0100

    added HerdManager class

--- app/src/main/java/mooc/vandy/java4android/gate/logic/Gate.java
+++ app/src/main/java/mooc/vandy/java4android/gate/logic/Gate.java
@@ -3,8 +3,97 @@ package mooc.vandy.java4android.gate.logic;
 /**
  * This file defines the Gate class.
  */
-public class Gate {
-    // TODO -- Fill in your code here
+public class Gate
+{
+    // Variables declaration
+    public static final int OUT = -1; //indicates outward door swing
+    public static final int IN = 1; //indicates inward door swing
+    public static final int CLOSED = 0; //indicates neither inward nor outward door swing
 
+    // Field declaration
+    private int mSwing;
 
+    // Public constructor to instantiate mSwing value
+    public Gate()
+    {
+        mSwing = CLOSED;
+    }
+
+    // Setter method
+    public boolean setSwing(int direction)
+    {
+        if (direction == IN || direction == OUT || direction == CLOSED)
+        {
+            mSwing = direction;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    // method that opens the gate
+    public boolean open(int direction)
+    {
+        if (direction == IN || direction == OUT)
+        {
+            this.setSwing(direction);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    // method that closes the gate
+    public void close()
+    {
+        mSwing = 0;
+    }
+
+    // Getter method
+    public int getSwingDirection()
+    {
+        return mSwing;
+    }
+
+    // method that performs passing through the gate
+    public int thru(int count)
+    {
+        if (mSwing == IN)
+        {
+            return count;
+        }
+        else if (mSwing == OUT)
+        {
+            return -count;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    // Overriding the toString method
+    public String toString()
+    {
+        if (mSwing == 0)
+        {
+            return "This gate is closed";
+        }
+        else if (mSwing == IN)
+        {
+            return "This gate is open and swings to enter the pen only";
+        }
+        else if (mSwing == OUT)
+        {
+            return "This gate is open and swings to exit the pen only";
+        }
+        else
+        {
+            return "This gate has an invalid swing direction";
+        }
+    }
 }
--- app/src/main/java/mooc/vandy/java4android/gate/logic/HerdManager.java
+++ app/src/main/java/mooc/vandy/java4android/gate/logic/HerdManager.java
@@ -9,7 +9,8 @@ import mooc.vandy.java4android.gate.ui.OutputInterface;
  * have supplied you will the code necessary to execute as an app.
  * You must fill in the missing logic below.
  */
-public class HerdManager {
+public class HerdManager
+{
     /**
      * Reference to the output.
      */
@@ -18,24 +19,29 @@ public class HerdManager {
     /**
      * The input Gate object.
      */
+    // OUT GATE
     private Gate mEastGate;
 
     /**
      * The output Gate object.
      */
+    // IN GATE
     private Gate mWestGate;
 
     /**
      * Maximum number of iterations to run the simulation.
      */
     private static final int MAX_ITERATIONS = 10;
+    public static final int HERD = 24;
 
     /**
      * Constructor initializes the fields.
      */
+    // CONSTRUCTOR
     public HerdManager(OutputInterface out,
                        Gate westGate,
-                       Gate eastGate) {
+                       Gate eastGate)
+    {
         mOut = out;
 
         mWestGate = westGate;
@@ -45,7 +51,60 @@ public class HerdManager {
         mEastGate.open(Gate.OUT);
     }
 
-    // TODO -- Fill your code in here
+    public void simulateHerd(Random rand)
+    {
+        /**
+         * Local variable equals to the size of HERD
+         */
+        int pen = HERD;
 
+        int pasture = 0;
+        int randomNumber = 0;
 
+        mOut.println("There are currently "+pen+ " snails in the pen and "+pasture+ " snails in" +
+                " the pasture");
+
+        /**
+         * Setting the number of iterations for the method
+         */
+        for (int i = 0; i < MAX_ITERATIONS; i++)
+        {
+            int direction = 0;
+            int count = 0;
+            boolean bool;
+
+            if (pen == 0)
+            {
+                randomNumber = rand.nextInt(pasture) + 1;
+                count = mWestGate.thru(randomNumber);
+                pen += count;
+            }
+            else if (pasture == 0)
+            {
+                randomNumber = rand.nextInt(pen) + 1;
+                count = mEastGate.thru(randomNumber);
+                pen += count;
+            }
+            else
+            {
+                bool = rand.nextBoolean();
+                if (bool)
+                {
+                    randomNumber = rand.nextInt(pen) + 1;
+                    count = mEastGate.thru(randomNumber);
+                    pen += count;
+                }
+                else
+                {
+                    randomNumber = rand.nextInt(pasture) + 1;
+                    count = mWestGate.thru(randomNumber);
+                    pen += count;
+                }
+            }
+
+            pasture = HERD - pen;
+            mOut.println("There are currently "+pen+ " snails in the pen and "+pasture+
+                    " snails in the pasture");
+        }
+    }
 }
